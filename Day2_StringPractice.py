33a69d8a3fd8dd9b052b8a9ceebcddbf69888069
gokuld27 <85781355+gokuld27@users.noreply.github.com>
2021-06-13 20:52:29 +0530

    Add files via upload

---
+++ Day2_StringPractice.py
@@ -0,0 +1,49 @@
+#Day 2 - Python String Practice
+#Gokul Dhakshana Murthy
+
+#How to print a value?
+print('Hello! This is Gokul Dhakshana Murthy')
+
+#Assigning String to variable
+Hello='Gokul Dhakshana Murthy'
+print(Hello)
+
+#Indexing using String
+Days='Gokul Dhakshana Murthy'
+print(Days[12])
+
+#How to print the particular character from certain text?
+Buffay='Princess Consuela Bananahammock'
+print(Buffay[3:10])
+
+#Print the length of Character
+print(len(Buffay))
+
+#Covert String into lower Character
+Less='ARE YOU READY?'
+print(Less.lower())
+
+#String Concatenation
+intro='Gokul'
+Duction='Dhakshana'
+print(intro + Duction)
+
+#Adding Space During concatenation
+intro='Gokul'
+Duction='Dhakshana'
+print(intro + ' ' +Duction)
+
+#casefold() usage
+print(Buffay.casefold())
+
+#capitalize() usage
+print(Buffay.capitalize())
+
+#find() usage
+print(Buffay.find('Cons'))
+
+#isalpha() usage
+print(Buffay.isalpha())
+
+#isalnum() usage
+print(Buffay.isalnum())
