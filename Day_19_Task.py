b661e2f25b6a60132804477e4f181f3e3d561ebb
gokuld27 <85781355+gokuld27@users.noreply.github.com>
2021-07-06 00:10:35 +0530

    Day 19 SQL - Filter & Sort

--- Day_19_Task.py
+++ Day_19_Task.py
@@ -4,7 +4,7 @@ try:
     mydb = mysql.connector.connect(
       host="localhost",
       user="root",
-      password="*********",
+      password="********",
       database="free",
       buffered = True,
     )
